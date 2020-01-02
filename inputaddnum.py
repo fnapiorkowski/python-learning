1cdee0714bc5a2ee67c241ad8092b7d05048d81d
fnapiorkowski <58856120+fnapiorkowski@users.noreply.github.com>
2020-01-02 14:00:21 +0000

    UI Add Numbers
    Adding Numbers with the User Input from the console.

---
+++ inputaddnum.py
@@ -0,0 +1,13 @@
+#python program to add 2 numbers with the user input
+number1 = input("First Number:" )
+number2 = input("\nSecond Number:")
+
+#Adding two numbers
+#User might insert float numbers
+sum = float(number1) + float(number2)
+
+#Display the sum
+#Will print the value in float
+print("The sum of {0} and {1} is {2}" .format(number1,number2, sum))
+
+
